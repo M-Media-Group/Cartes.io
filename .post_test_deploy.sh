dc60c6790e6136f851efc7051edc0a4c2699d98e
Michał Wargan <mwargan@pc61.home>
2019-01-24 14:12:35 +0100

    Travis commits

---
+++ .post_test_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Trigger deployment
+curl -s 'https://forge.laravel.com/servers/230537/sites/695379/deploy/http?token=2ixMSuLEYJLaB3yWiXLMFIqqaWXAanvWfJmUT0LE';
+echo 'Deployment triggered!'
