57994909f765a21ce221370fe2418e9f624a5c47
Michał Wargan <mwargan@pc61.home>
2019-01-26 17:41:30 +0100

    Better post sorting

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
 		<a href="/posts/{{$post->slug}}" title="{{ $post->title }}">
 		    <img src="{{$post->header_image}}" class="rounded img-thumbnail mb-2" alt="{{ $post->title }}" >
 		    <h2>{{ $post->title }}</h2>
-		    <p class="text-muted">{{ $post->excerpt }} | {{$post->views_count}}</p>
+		    <p class="text-muted">{{ $post->excerpt }}</p>
 		    <p class="text-muted">
 		    	@foreach($post->categories as $category)
 			        <a href="/categories/{{$category->slug}}"><img class="rounded img-thumbnail mr-1" height="30" width="30" src="{{$category->icon}}" alt="{{$category->name}}">{{$category->name}}</a>
