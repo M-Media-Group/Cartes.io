c45a50686ea2773649976091ce775c6a7e4ea8de
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-04-27 20:22:34 +0200

    Updates

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -16,7 +16,7 @@ class VerificationController extends Controller
     | user that recently registered with the application. Emails may also
     | be re-sent if the user didn't receive the original email message.
     |
-    */
+     */
 
     use VerifiesEmails;
 
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,11 +23,12 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        return redirect('/');
+
         $categories = $request->user()->seenCategories()->groupBy('category_id', 'category_views.user_id')->orderByRaw('MAX(category_views.created_at) DESC')->get();
         $posts = $request->user()->seenPosts()->groupBy('post_id', 'post_views.user_id')->orderByRaw('MAX(post_views.created_at) DESC')->get();
 
-        return view('home', compact('posts', 'categories'));
+        //return view('home', compact('posts', 'categories'));
 
-        //return view('home');
     }
 }
