669c4d4497698a1cbedadb08173a74e652deee90
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-04-28 13:01:56 +0200

    Updated consent notice

---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,15 @@
+<div class="js-cookie-consent cookie-consent alert alert-primary alert-dismissible" role="alert" style="position: fixed;bottom: 0;z-index: 99999;">
+
+    <span class="cookie-consent__message">
+        {!! trans('cookieConsent::texts.message') !!}
+    </span>
+
+    <button class="btn btn-primary js-cookie-consent-agree cookie-consent__agree">
+        {{ trans('cookieConsent::texts.agree') }}
+    </button>
+
+	<button type="button" class="btn close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</button>
+
+</div>
