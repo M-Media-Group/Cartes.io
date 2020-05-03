49e530eceaf5f831b63aab7824a2192448e9f003
mwargan <mwargan@gmail.com>
2020-05-03 22:00:52 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -81,7 +81,7 @@ class CategoryController extends Controller
     public function show(Request $request, $slug)
     {
         $category = Category::where('slug', $slug)->with('incidents')->firstOrFail();
-        if (!$request->user()) {
+        if (! $request->user()) {
             $user_id = null;
         } else {
             $user_id = $request->user()->id;
@@ -136,7 +136,7 @@ class CategoryController extends Controller
             ]
         );
 
-        return redirect('/categories/' . str_slug($request->input('name')));
+        return redirect('/categories/'.str_slug($request->input('name')));
     }
 
     /**
