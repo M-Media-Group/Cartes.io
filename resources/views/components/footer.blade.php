7db79a3e24b387b31af740f938f386973acf9765
Michał Wargan <mwargan@michas-macbook-pro.home>
2020-05-04 19:12:20 +0200

    Added rebrand notice

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,6 @@
-
+<div class="alert alert-primary" role="alert">
+<b>Incident Tracker</b> will be rebranded as <b>Cartes</b> and moved to <a href="https://cartes.io">cartes.io</a> on the <b>8th of May</b>.
+</div>
 		@include('cookieConsent::index')
 
 		<script src="{{ mix('js/manifest.js') }}" defer></script>
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <h1>Privacy Policy</h1>
+
+<h2>In plain ol' English</h2>
+<ol>
+<li>When you create a map and you are logged out, none of your personal information is stored on our servers.</li>
+<li>When you create a marker/incident on the map and you are logged out, none of your personal information is stored on our servers.</li>
+<li>When you are logged in, we associate any actions you take and rescources you make (like creating a map or a marker) with your account.</li>
+<li>When you consent to cookies, we share personally identifying info (such as your IP address) with Google Analytics and Facebook. This data is NOT stored on our servers. Rest assured we're not interested in your individual data, we look for aggregate data and patterns on the website which helps us continously improve everyones experience. You do not need to accept cookies for this website to function.</li>
+</ol>
+<h2>Original policy text</h2>
 <p>Your privacy is important to us. It is {{ config('app.name') }}'s policy to respect your privacy regarding any information we may collect from you across our website, <a href="{{ config('app.url') }}">{{ config('app.url') }}</a>, and other sites we own and operate.</p>
 <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
 <p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
