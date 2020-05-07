304b1f852852765bf4ff6608047b449c780207e4
mwargan <mwargan@gmail.com>
2020-05-07 17:39:03 +0000

    Apply fixes from StyleCI

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,5 @@ class DatabaseSeeder extends Seeder
         //$this->call(UsersTableSeeder::class);
         $this->call(MapsTableSeeder::class);
         //$this->call(IncidentsTableSeeder::class);
-
     }
 }
