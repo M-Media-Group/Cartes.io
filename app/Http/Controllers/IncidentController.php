f309c6c0523a21d60c781cc0d49795d8f7b295ba
mwargan <mwargan@gmail.com>
2020-05-29 15:18:07 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -25,6 +25,7 @@ class IncidentController extends Controller
             $data = $data->withoutGlobalScope('active');
         }
         $data = $data->with('category')->get();
+
         return $data;
     }
 
@@ -52,7 +53,7 @@ class IncidentController extends Controller
             'user_id' => 'nullable|exists:users,id',
         ]);
 
-        if (!$request->input('category')) {
+        if (! $request->input('category')) {
             $category = \App\Models\Category::firstOrCreate(
                 ['slug' => str_slug($request->input('category_name'))],
                 ['name' => $request->input('category_name'), 'icon' => '/images/marker-01.svg']
