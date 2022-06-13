85d79345278b06292182979d7f047d58a64f037b
Michal <mwargan@gmail.com>
2022-06-13 12:52:46 +0000

    Wrapped in layout

--- resources/views/ar/map.blade.php
+++ resources/views/ar/map.blade.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.clean', ['index_and_googlebots' => $map->privacy == 'public' ? true : false])
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>AR Demo</title>
+@section('title', $map->title ?? 'Untitled map')
+@section('meta_description', $map->description ?? 'No map description')
+@section('meta_image', config('app.url') . '/images/map.png')
+
+@section('header_scripts')
     <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
     <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
-</head>
+@endsection
 
-<body style="margin: 0; overflow: hidden;">
+@section('above_container')
     <a-scene vr-mode-ui="enabled: false" arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;">
         @foreach ($map->markers as $marker)
             <a-text value="{{ $marker->category->name }}" look-at="[gps-camera]" scale="70 70 70"
@@ -20,6 +20,4 @@
         <a-camera gps-camera rotation-reader>
         </a-camera>
     </a-scene>
-</body>
-
-</html>
+@endsection
