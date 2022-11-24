931aeeff8bdcfb16e970bbc51d6b9e3f874ca5dc
Michal <mwargan@gmail.com>
2022-11-24 15:09:08 +0000

    Switched to sync broadcasting

--- app/Events/MarkerCreated.php
+++ app/Events/MarkerCreated.php
@@ -5,10 +5,10 @@ namespace App\Events;
 use App\Models\Marker;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
 
-class MarkerCreated implements ShouldBroadcast
+class MarkerCreated implements ShouldBroadcastNow
 {
     use SerializesModels;
     use InteractsWithSockets;
