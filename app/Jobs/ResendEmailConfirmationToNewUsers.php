11999d97cb837a7f8f3c72d6bebe6cce8b5b0d81
Michal <mwargan@gmail.com>
2022-11-24 12:48:30 +0000

    Removed unused use

--- app/Jobs/ResendEmailConfirmationToNewUsers.php
+++ app/Jobs/ResendEmailConfirmationToNewUsers.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
