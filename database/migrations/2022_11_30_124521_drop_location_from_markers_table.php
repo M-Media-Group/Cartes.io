e605e2d84e368ad75102c93443c0830d496d9beb
Michal <mwargan@gmail.com>
2022-11-30 12:30:14 +0000

    Updated spacial index drop

--- database/migrations/2022_11_30_124521_drop_location_from_markers_table.php
+++ database/migrations/2022_11_30_124521_drop_location_from_markers_table.php
@@ -14,13 +14,13 @@ class DropLocationFromMarkersTable extends Migration
     public function up()
     {
         Schema::table('markers', function (Blueprint $table) {
-            $table->dropColumn('elevation');
             /**
              * @note that the index may be called incidents_location_spatial rather than incidents_location_spatialindex locally
              */
             $table->dropIndex('incidents_location_spatialindex');
             $table->dropUnique('markers_location_map_id_category_id_created_at_expires_at_unique');
             $table->dropColumn('location');
+            // $table->dropColumn('elevation');
         });
     }
 
