489c93a3e6a3e8eaf079f75237d4023d93cd363f
Michal <mwargan@gmail.com>
2022-11-30 12:48:13 +0000

    Added check for index

--- database/migrations/2022_11_30_124521_drop_location_from_markers_table.php
+++ database/migrations/2022_11_30_124521_drop_location_from_markers_table.php
@@ -17,7 +17,18 @@ class DropLocationFromMarkersTable extends Migration
             /**
              * @note that the index may be called incidents_location_spatial rather than incidents_location_spatialindex locally
              */
-            $table->dropIndex('incidents_location_spatialindex');
+            $sm = Schema::getConnection()->getDoctrineSchemaManager();
+            $indexesFound = $sm->listTableIndexes('markers');
+
+            if (array_key_exists("incidents_location_spatialindex", $indexesFound)) {
+                $table->dropIndex('incidents_location_spatialindex');
+            }
+
+            if (array_key_exists("incidents_location_spatial", $indexesFound)) {
+                $table->dropIndex('incidents_location_spatial');
+            }
+
+
             $table->dropUnique('markers_location_map_id_category_id_created_at_expires_at_unique');
             $table->dropColumn('location');
             $table->dropColumn('elevation');
