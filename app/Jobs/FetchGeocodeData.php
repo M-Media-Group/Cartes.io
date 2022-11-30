191a71b6cbeb036f26634e51b335edef59e7340a
Michal <mwargan@gmail.com>
2022-11-30 13:26:57 +0000

    Moved fetching geocode data to secondary queuable job

---
+++ app/Jobs/FetchGeocodeData.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class FetchGeocodeData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $location;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(\App\Models\MarkerLocation $location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = new Client(['headers' => ['Accept-Language' => 'en']]);
+
+        try {
+            $response = $client->get('https://nominatim.openstreetmap.org/reverse?format=geojson&lat=' . $this->location->y . '&lon=' . $this->location->x);
+
+            // If there was an error, stop
+            if ($response->getStatusCode() !== 200) {
+                // Throw an exception
+                throw new \Exception('Error calling Nominatim API - returned code ' . $response->getStatusCode());
+            }
+
+            $geocodeResult = json_decode($response->getBody()->getContents(), false);
+
+            // This usually happens when a marker is in the middle of nowhere in the ocean
+            if (property_exists($geocodeResult, 'error')) {
+                $this->location->geocode = $geocodeResult ?? [];
+                $this->location->save();
+                return;
+            }
+
+            $this->location->address = optional($geocodeResult->features)[0]->properties->display_name ?? null;
+
+            // Note that we use an empty array if no results are found - this is because we need to change the value from NULL to something else to know that we have already attempted to geocode the given location
+            $this->location->geocode = $geocodeResult ?? [];
+
+            $this->location->save();
+        } catch (\Throwable $th) {
+            Log::error("Could not get Nominatim data", [$th]);
+        }
+    }
+}
--- app/Jobs/FillMissingLocationGeocodes.php
+++ app/Jobs/FillMissingLocationGeocodes.php
@@ -58,36 +58,10 @@ class FillMissingLocationGeocodes implements ShouldQueue
                 continue;
             }
 
-            try {
-                $response = $client->get('https://nominatim.openstreetmap.org/reverse?format=geojson&lat=' . $location->y . '&lon=' . $location->x);
+            FetchGeocodeData::dispatch($location);
 
-                // If there was an error, stop
-                if ($response->getStatusCode() !== 200) {
-                    // Throw an exception
-                    throw new \Exception('Error calling Nominatim API - returned code ' . $response->getStatusCode());
-                }
-
-                $geocodeResult = json_decode($response->getBody()->getContents(), false);
-
-                // This usually happens when a marker is in the middle of nowhere in the ocean
-                if (property_exists($geocodeResult, 'error')) {
-                    $location->geocode = $geocodeResult ?? [];
-                    $location->save();
-                    continue;
-                }
-
-                $location->address = optional($geocodeResult->features)[0]->properties->display_name ?? null;
-
-                // Note that we use an empty array if no results are found - this is because we need to change the value from NULL to something else to know that we have already attempted to geocode the given location
-                $location->geocode = $geocodeResult ?? [];
-
-                $location->save();
-
-                // Wait one second before the next request, in accordance with the API usage policy
-                sleep(1);
-            } catch (\Throwable $th) {
-                Log::error("Could not get Nominatim data", [$th]);
-            }
+            // Wait one second before the next request, in accordance with the API usage policy
+            sleep(1);
         }
     }
 }
