440f9566077f59a95781bf8560664b5ea5dfd5f2
Michal <mwargan@gmail.com>
2023-01-16 12:07:03 +0000

    Added socketi config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,6 +41,20 @@ return [
             ],
         ],
 
+        'socketi' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY', 'app-key'),
+            'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
+            'app_id' => env('PUSHER_APP_ID', 'app-id'),
+            'options' => [
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
+            ],
+        ],
+
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
